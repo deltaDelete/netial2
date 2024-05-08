fd3c8bf7435686cecc3ffa64874ede74d6ee3438
deltaDelete <samansy@yandex.ru>
2024-05-09 06:45:00 +0700

    test: add GET /users test and schema check

--- src/test/kotlin/ru/deltadelete/netial/ApplicationTest.kt
+++ src/test/kotlin/ru/deltadelete/netial/ApplicationTest.kt
@@ -1,39 +1,77 @@
 package ru.deltadelete.netial
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.testing.*
-import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
-import ru.deltadelete.netial.plugins.InstantDeserializer
-import ru.deltadelete.netial.plugins.InstantSerializer
-import ru.deltadelete.netial.plugins.configureRouting
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
+import ru.deltadelete.netial.database.dao.User
+import ru.deltadelete.netial.database.dto.UserDto
+import ru.deltadelete.netial.database.schemas.Permission
+import ru.deltadelete.netial.plugins.*
+import ru.deltadelete.netial.routes.users.configureUsers
+import java.util.*
 import kotlin.test.*
 
 class ApplicationTest {
+
     @Test
-    fun testRoot() = testApplication {
+    fun testDatabase() = testApplication {
         application {
-            configureRouting()
+            configureDatabases()
+            assertNotNull(TransactionManager.defaultDatabase)
         }
-        client.get("/").apply {
+    }
+
+    @Test
+    fun testUsersGet() = testApplication {
+        val mapper = mapper()
+        application {
+            configureDatabases()
+            configureSecurity()
+            configureSerialization()
+            configureUsers()
+        }
+        client.get("/users") {
+            header(HttpHeaders.Accept, ContentType.Application.Json)
+        }.apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            val body = bodyAsText()
+            assertNotEquals("", body, "Body is empty")
+
+            val users = mapper.readValue<List<UserDto>>(body)
+            assertTrue {
+                users.isNotEmpty()
+            }
+        }
+    }
+
+    @Test
+    fun testUsersSchemaRoles() = testApplication {
+        application {
+            configureDatabases()
+            transaction {
+                assertTrue {
+                    val user = User.all().firstOrNull()
+                    val roles = user?.roles
+                    println(roles?.first()?.permissions)
+                    return@assertTrue roles?.any() ?: false
+                }
+            }
         }
     }
 
     @Test
     fun testInstantSerializer() {
-        val mapper = jacksonObjectMapper()
-            .registerModules(
-                SimpleModule()
-                    .addSerializer(Instant::class.java, InstantSerializer())
-                    .addDeserializer(Instant::class.java, InstantDeserializer())
-            )
+        val mapper = mapper()
         val input = Instant.fromEpochSeconds(1097712000)
         val expected = "1097712000"
 
@@ -44,12 +82,7 @@ class ApplicationTest {
 
     @Test
     fun testInstantDeserializer() {
-        val mapper = jacksonObjectMapper()
-            .registerModules(
-                SimpleModule()
-                    .addSerializer(Instant::class.java, InstantSerializer())
-                    .addDeserializer(Instant::class.java, InstantDeserializer())
-            )
+        val mapper = mapper()
         val input = "1097712000"
         val expected = Instant.fromEpochSeconds(1097712000)
 
@@ -57,4 +90,47 @@ class ApplicationTest {
 
         assertEquals(expected.epochSeconds, output.epochSeconds)
     }
+
+    @Test
+    fun testEnumSetSerializer() {
+        val mapper = jacksonObjectMapper()
+        val input = EnumSet.of(Permission.CREATE_ROLE, Permission.MODIFY_ROLE)
+        val expected = """
+            ["CREATE_ROLE","MODIFY_ROLE"]
+        """.trimIndent()
+
+        val output = mapper.writeValueAsString(input)
+
+        assertEquals(expected, output)
+    }
+
+    @Test
+    fun testEnumSetDeserializer() {
+        val mapper = jacksonObjectMapper()
+        val input = """
+            ["CREATE_ROLE","MODIFY_ROLE"]
+        """.trimIndent()
+        val expected = EnumSet.of(Permission.CREATE_ROLE, Permission.MODIFY_ROLE)
+
+        val output = mapper.readValue<EnumSet<Permission>>(input)
+
+        assertEquals(expected, output)
+    }
 }
+
+fun Application.configureTestDatabases() {
+    // in memory h2
+    val database = Database.connect(
+        url = "jdbc:h2:mem:test",
+        driver = "org.h2.Driver",
+        user = "sa",
+        password = ""
+    )
+}
+
+fun mapper(): ObjectMapper = jacksonObjectMapper()
+    .registerModules(
+        SimpleModule()
+            .addSerializer(Instant::class.java, InstantSerializer())
+            .addDeserializer(Instant::class.java, InstantDeserializer())
+    )
